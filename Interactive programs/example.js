17a0ffbffdbfe97b0373304fa020b38915ad7c07
Mage <magnus.liivamagi@gmail.com>
2022-11-07 19:32:14 +0200

    Initial commit

---
+++ Interactive programs/example.js
@@ -0,0 +1,8 @@
+'use strict';
+console.log('Howdy partner!');
+alert('Hi from here too!');
+let name = prompt("gib name");
+document.querySelector('#target').innerHTML = 'Good morning, ' + name + '!';
+let x = prompt("Enter int:");
+x = parseInt(x);
+document.querySelector('#test').innerHTML = x;
