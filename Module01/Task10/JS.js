e2764caa3f21c5f46500633edb5b4ed1d2f01f20
Mage <magnus.liivamagi@gmail.com>
2022-11-09 14:29:22 +0200

    fixt mod1 task 10

--- Module01/Task10/JS.js
+++ Module01/Task10/JS.js
@@ -1,15 +1,16 @@
 const dicenum = parseInt(prompt('HOW MANY DICE!?!??!?!?!!!!!!!'));
 const goal = parseInt(prompt('HAU many eyz you wants?'));
+const numofrounds = 10000000
 let goalachieved = 0;
 let sum = 0;
 
-for (throws = 0; throws <= 1000000; throws++) {
+for (throws = 0; throws <= numofrounds; throws++) {
   for (i = 0; i < dicenum; i++) {
-    sum += Math.floor((Math.random() * 6) + 1);
-    if (sum == goal) {
+    sum = sum + Math.floor((Math.random() * 6) + 1);
+  }
+  if (sum == goal) {
       goalachieved++;
     }
-  }
   sum = 0;
 }
-document.querySelector('#print').textContent = `Probability to get sum ${goal} with ${dicenum} dice is ${((goalachieved / 10000) * 100).toFixed(5)}%`
+document.querySelector('#print').textContent = `Probability to get sum ${goal} with ${dicenum} dice is ${((goalachieved / numofrounds) * 100).toFixed(5)}%`
